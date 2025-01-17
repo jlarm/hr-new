2e5e8ba98e967720f93679905436ee30e99cd5b8
jlarm <117611704+jlarm@users.noreply.github.com>
2025-01-17 11:15:54 -0600

    App template components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'ARMP HR' }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
@@ -24,95 +24,29 @@
         >
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-            <flux:brand
-                href="#"
-                logo="https://fluxui.dev/img/demo/logo.png"
-                name="Acme Inc."
-                class="px-2 dark:hidden"
-            />
-            <flux:brand
-                href="#"
-                logo="https://fluxui.dev/img/demo/dark-mode-logo.png"
-                name="Acme Inc."
-                class="hidden px-2 dark:flex"
-            />
+            <div class="w-full">
+                <x-application-logo class="h-8 w-auto" />
+            </div>
 
-            <flux:input as="button" variant="filled" placeholder="Search..." icon="magnifying-glass" />
-
-            <flux:navlist variant="outline">
-                <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
-                <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
-                <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
-                <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
-
-                <flux:navlist.group expandable heading="Favorites" class="hidden lg:grid">
-                    <flux:navlist.item href="#">Marketing site</flux:navlist.item>
-                    <flux:navlist.item href="#">Android app</flux:navlist.item>
-                    <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
-                </flux:navlist.group>
-            </flux:navlist>
+            <x-navigation />
 
             <flux:spacer />
 
-            <flux:navlist variant="outline">
-                <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
-                <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
-            </flux:navlist>
-
-            <flux:dropdown position="top" align="start" class="max-lg:hidden">
-                <flux:profile avatar="https://fluxui.dev/img/demo/user.png" name="Olivia Martin" />
-
-                <flux:menu>
-                    <flux:menu.radio.group>
-                        <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                        <flux:menu.radio>Truly Delta</flux:menu.radio>
-                    </flux:menu.radio.group>
-
-                    <flux:menu.separator />
-
-                    <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
-                </flux:menu>
-            </flux:dropdown>
+            <x-user-dropdown />
         </flux:sidebar>
 
         <flux:header
             class="!block border-b border-zinc-200 bg-white lg:bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900"
         >
-            <flux:navbar class="w-full lg:hidden">
-                <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+            <x-mobile-navigation />
 
-                <flux:spacer />
-
-                <flux:dropdown position="top" align="start">
-                    <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
-
-                    <flux:menu>
-                        <flux:menu.radio.group>
-                            <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                            <flux:menu.radio>Truly Delta</flux:menu.radio>
-                        </flux:menu.radio.group>
-
-                        <flux:menu.separator />
-
-                        <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
-                    </flux:menu>
-                </flux:dropdown>
-            </flux:navbar>
-
-            <flux:navbar scrollable>
-                <flux:navbar.item href="#" current>Dashboard</flux:navbar.item>
-                <flux:navbar.item badge="32" href="#">Orders</flux:navbar.item>
-                <flux:navbar.item href="#">Catalog</flux:navbar.item>
-                <flux:navbar.item href="#">Configuration</flux:navbar.item>
-            </flux:navbar>
+            <x-sub-navigation />
         </flux:header>
 
         <flux:main>
-            <flux:heading size="xl" level="1">Good afternoon, Olivia</flux:heading>
-
-            <flux:subheading size="lg" class="mb-6">Here's what's new today</flux:subheading>
-
-            <flux:separator variant="subtle" />
+            @if (request()->routeIs('dashboard'))
+                <x-welcome-message />
+            @endif
 
             <main>
                 {{ $slot }}
---
+++ resources/views/components/sub-navigation.blade.php
@@ -0,0 +1,6 @@
+<flux:navbar scrollable>
+    <flux:navbar.item href="#" current>Dashboard</flux:navbar.item>
+    <flux:navbar.item badge="32" href="#">Orders</flux:navbar.item>
+    <flux:navbar.item href="#">Catalog</flux:navbar.item>
+    <flux:navbar.item href="#">Configuration</flux:navbar.item>
+</flux:navbar>
